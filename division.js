40b0cdd7a25959610bb83d39f2441ccf53e2de2b
Saleem3097 <saleem3097@gmail.com>
2022-04-14 05:47:39 +0530

    div and sub added feature in sub_div

---
+++ division.js
@@ -0,0 +1,6 @@
+function div(){
+var a = document.getElementById("inp1").value;
+var b = document.getElementById("inp2").value;
+var res = a/b;
+document.getElementById("res").value =  res;
+}
---
+++ subtraction.js
@@ -0,0 +1,7 @@
+function sub(){
+var a = document.getElementById("inp1").value;
+var b = document.getElementById("inp2").value;
+var res = a-b;
+document.getElementById("res").value =  res;
+}
+
