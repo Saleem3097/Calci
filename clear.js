4dcf4d45b1f420a79c3281c2ec86ae40f5d480db
Saleem3097 <saleem3097@gmail.com>
2022-04-14 07:31:35 +0530

    final modification

--- clear.js
+++ clear.js
@@ -1,6 +1,6 @@
 function clear(){
     inputs=document.getElementsByClassName('inputs')
     for(input of inputs){
-        input.value = ' ' //this empties rthe input
+        input.value = ' '//this empties the input
     }
 }
