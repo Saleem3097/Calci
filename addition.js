a464cc103565ec99fbfbf104f65c3f4759192133
Saleem3097 <saleem3097@gmail.com>
2022-04-14 13:02:15 +0530

    modified addition file

--- addition.js
+++ addition.js
@@ -1,7 +1,7 @@
 function add() {
 var a = document.getElementById("inp1").value;
 var b = document.getElementById("inp2").value;
-var res = parseInt(a)+parseInt(b);
+var res = parseInt(a) + parseInt(b);
 document.getElementById("res").value =  res;
 }
 
