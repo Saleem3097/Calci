a3e16521621bd09c600af8d054aae42fd31283af
Saleem3097 <saleem3097@gmail.com>
2022-04-15 09:45:22 +0530

    rectified subtraction program functionality

--- subyes.js
+++ subyes.js
@@ -1,6 +1,6 @@
-function multi(){
+function subYes(){
 var a = document.getElementById("inp1").value;
 var b = document.getElementById("inp2").value;
-var res = (parseInt(a)*parseInt(b));
+var res = parseInt(a) - parseInt(b);
 document.getElementById("res").value =  res;
 }
